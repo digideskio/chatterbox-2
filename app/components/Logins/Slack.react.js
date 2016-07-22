0e8fb78a249308d9e5d1b709a50bf94ed22f6593
Luigi Poole <luigipoole@outlook.com>
2016-07-22 16:25:15 -0700

    further slack login work

--- app/components/Logins/Slack.react.js
+++ app/components/Logins/Slack.react.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
+import url from 'url'
+import qs from 'qs'
 import { shell } from 'electron'
 import styles from 'styles/login.css'
 
@@ -12,6 +14,14 @@ export default class SlackLogin extends Component {
 
   componentDidMount() {
     this.mounted = true
+    this._initWebviewEvents()
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+  }
+
+  _initWebviewEvents() {
     const { webview } = this.refs
 
     webview.addEventListener('dom-ready', () => {
@@ -23,16 +33,31 @@ export default class SlackLogin extends Component {
 
     webview.addEventListener('will-navigate', (event) => {
       if (!this.mounted) return
-      console.log(event)
       if (event.url.endsWith('/forgot')) {
         shell.openExternal(event.url)
+        webview.stop()
+      }
+      if (event.url.startsWith('http://localhost')) {
+        const { code } = qs.parse(url.parse(event.url).query)
+        console.log(code)
         webview.stop()
       }
     })
   }
 
-  componentWillUnmount() {
-    this.mounted = false
+  get oAuthQuery() {
+    return qs.stringify({
+      client_id: '8772351907.62016425399',
+      scope: [
+        'team:read',
+        'channels:history', 'channels:read',
+        'im:history', 'im:read', 'im:write',
+        'chat:write:user',
+        'users:read', 'users:write', 'users.profile:read'
+      ].join(','),
+      redirect_uri: 'http://localhost',
+      repick: 1
+    })
   }
 
   render() {
@@ -42,8 +67,8 @@ export default class SlackLogin extends Component {
         <div className={classnames(styles.sidebar, styles.full)}>
           <object className={styles.slack} data='images/logins/slack.svg' type='image/svg+xml'/>
         </div>
-        <div className={classnames(styles.webview, {[styles.show]: this.state.webviewShown})}>
-          <webview ref='webview' src='https://slack.com/oauth/pick_reflow?client_id=8772351907.62016425399&scope=channels%3Aread&redirect_uri=http%3A%2F%2Flocalhost'/>
+        <div className={classnames(styles.webview, {[styles.show]: webviewShown})}>
+          <webview ref='webview' src={`https://slack.com/oauth/pick_reflow?${this.oAuthQuery}`}/>
         </div>
       </div>
     )
