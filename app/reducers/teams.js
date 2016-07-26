502fd9ff08db41add2fd3e1e52fd633aa0ae0bfc
Luigi Poole <luigipoole@outlook.com>
2016-07-26 01:03:40 -0700

    add ionicons to vender folder

--- app/reducers/teams.js
+++ app/reducers/teams.js
@@ -4,6 +4,7 @@ import {
   TEAM_REMOVE,
 
   NEW_MESSAGE,
+  MODIFY_MESSAGE,
   EDIT_MESSAGE,
   REMOVE_MESSAGE
 } from 'actions/teams'
@@ -41,12 +42,13 @@ const DEFAULT_STATE = {
 export default function settings(state = DEFAULT_STATE, { type, ...action }) {
   switch (type) {
     case TEAM_ADD:
-      return { ...state, teams: { ...state.teams, [action.team.team.id]: action.team }, activeTeamID: action.team.team.id }
+      return {...state, teams: {...state.teams, [action.team.team.id]: action.team }, activeTeamID: action.team.team.id }
     case ACTIVE_TEAM_CHANGE:
       return {...state, activeTeamID: action.activeTeamID }
     case NEW_MESSAGE:
       const { team: messageTeam, channel: messageChannel, ...message } = action.message
-      const { [messageTeam]: teamToChange, ...teams } = state.teams
+      const {
+        [messageTeam]: teamToChange, ...teams } = state.teams
 
       if (!teamToChange.messages[messageChannel]) {
         teamToChange.messages[messageChannel] = [message]
