a27ae50b6cedec62390c33e264cce1c975ff51fc
Luigi Poole <luigipoole@outlook.com>
2016-07-28 23:46:46 -0700

    split helper functions from slack handler

---
+++ app/lib/handlers/slack.helpers.js
@@ -0,0 +1,85 @@
+import moment from 'moment'
+import _ from 'lodash'
+import emojify from '../emojify'
+
+
+export function santitizeUser({ tz: timezone, id, deleted, profile, name: handle, presence }) {
+  return {
+    handle,
+    name: _.get(profile, 'real_name_normalized', '').length > 0 ? profile.real_name_normalized : null,
+    id,
+    presence: presence === 'active' ? 'online' : 'offline',
+    images: _.filter(profile, (data, key) => key.includes('image')),
+    meta: { timezone, email: _.get(profile, 'email') }
+  }
+}
+
+function santitizeAttachments(attachments) {
+  return attachments.map(({ title, text, pretext, ...attachment }) => {
+    return {
+      images: { thumb: attachment.thumb_url, author: attachment.author_icon },
+      links: { author: attachment.author_link, title: attachment.title_link },
+      author: attachment.author_name,
+      title,
+      pretext,
+      text
+    }
+  })
+}
+
+function formatText(text) {
+  let formatText = emojify(text)
+  return formatText
+}
+
+function santitizeMessage({ user, text, ts: timestamp, user_profile: userProfile = null, attachments = [] }) {
+  return {
+    attachments: santitizeAttachments(attachments),
+    user,
+    text: formatText(text),
+    userProfile,
+    timestamp,
+    friendlyTimestamp: moment.unix(timestamp).format('h:mm a')
+  }
+}
+
+export function parseMessage({ type, subtype, bot_id, channel = null, ...messageData }, overrideEvent = false) {
+  let isBot = Boolean(bot_id)
+  let userProfileChecked = false
+  switch (subtype ? `${type}:${subtype}` : type) {
+    case 'message:bot_message':
+      (() => {
+        isBot = true
+        userProfileChecked = true
+        const { images, icons, name: handle, id } = this._slack.dataStore.bots[bot_id]
+        messageData.user_profile = { handle, id, image: _.last(_.filter((images || icons), (a, key) => key.includes('image'))) }
+      })()
+    case 'message:file_share':
+    case 'message':
+      return (() => {
+        if (messageData.user_profile && !userProfileChecked) {
+          const { name: handle, real_name: name, ...user_profile } = messageData.user_profile
+          messageData.user_profile = {
+            image: _.last(_.filter(user_profile, (a, key) => key.includes('image') || key.includes('icon'))),
+            handle,
+            name
+          }
+        }
+
+        const msg = _.omitBy({ channel, isBot, ...santitizeMessage(messageData) }, _.isNil)
+        if (overrideEvent) return msg
+        else this.emit('message', msg)
+      })()
+    case 'message:message_changed':
+      return (() => {
+        const { message, event_ts: eventTimestamp, previous_message: { ts: previousMessageTimestamp } } = messageData
+        const msg = { channel, message: santitizeMessage(message), edit: { eventTimestamp, previousMessageTimestamp } }
+
+        if (overrideEvent) return msg
+        else this.emit('message:changed', msg)
+      })()
+    default:
+      console.info('Unable to parse message:', { type, subtype, ...messageData })
+      return false
+  }
+}
--- app/lib/handlers/slack.js
+++ app/lib/handlers/slack.js
@@ -1,8 +1,7 @@
 import { EventEmitter } from 'events'
-import moment from 'moment'
 import _ from 'lodash'
 import { WebClient, RtmClient, MemoryDataStore, CLIENT_EVENTS, RTM_EVENTS } from '@slack/client'
-import emojify from '../emojify'
+import { santitizeUser, parseMessage } from './slack.helpers'
 
 const DEFAULT_OPTIONS = {
   logLevel: 'error',
@@ -10,88 +9,6 @@ const DEFAULT_OPTIONS = {
   autoReconnect: true,
   autoJoinNewChannels: false
 }
-
-function santitizeUser({ tz: timezone, id, deleted, profile, name: handle, presence }) {
-  return {
-    handle,
-    name: _.get(profile, 'real_name_normalized', '').length > 0 ? profile.real_name_normalized : null,
-    id,
-    presence: presence === 'active' ? 'online' : 'offline',
-    images: _.filter(profile, (data, key) => key.includes('image')),
-    meta: { timezone, email: _.get(profile, 'email') }
-  }
-}
-
-function santitizeAttachments(attachments) {
-  return attachments.map(({ title, text, pretext, ...attachment }) => {
-    return {
-      images: { thumb: attachment.thumb_url, author: attachment.author_icon },
-      links: { author: attachment.author_link, title: attachment.title_link },
-      author: attachment.author_name,
-      title,
-      pretext,
-      text
-    }
-  })
-}
-
-function formatText(text) {
-  let formatText = emojify(text)
-  return formatText
-}
-
-function santitizeMessage({ user, text, ts: timestamp, user_profile: userProfile = null, attachments = [] }) {
-  return {
-    attachments: santitizeAttachments(attachments),
-    user,
-    text: formatText(text),
-    userProfile,
-    timestamp,
-    friendlyTimestamp: moment.unix(timestamp).format('h:mm a')
-  }
-}
-
-function parseMessage({ type, subtype, bot_id, channel = null, ...messageData }, overrideEvent = false) {
-  let isBot = Boolean(bot_id)
-  let userProfileChecked = false
-  switch (subtype ? `${type}:${subtype}` : type) {
-    case 'message:bot_message':
-      (() => {
-        isBot = true
-        userProfileChecked = true
-        const { images, icons, name: handle, id } = this._slack.dataStore.bots[bot_id]
-        messageData.user_profile = { handle, id, image: _.last(_.filter((images || icons), (a, key) => key.includes('image'))) }
-      })()
-    case 'message:file_share':
-    case 'message':
-      return (() => {
-        if (messageData.user_profile && !userProfileChecked) {
-          const { name: handle, real_name: name, ...user_profile } = messageData.user_profile
-          messageData.user_profile = {
-            image: _.last(_.filter(user_profile, (a, key) => key.includes('image') || key.includes('icon'))),
-            handle,
-            name
-          }
-        }
-
-        const msg = _.omitBy({ channel, isBot, ...santitizeMessage(messageData) }, _.isNil)
-        if (overrideEvent) return msg
-        else this.emit('message', msg)
-      })()
-    case 'message:message_changed':
-      return (() => {
-        const { message, event_ts: eventTimestamp, previous_message: { ts: previousMessageTimestamp } } = messageData
-        const msg = { channel, message: santitizeMessage(message), edit: { eventTimestamp, previousMessageTimestamp } }
-
-        if (overrideEvent) return msg
-        else this.emit('message:changed', msg)
-      })()
-    default:
-      console.info('Unable to parse message:', { type, subtype, ...messageData })
-      return false
-  }
-}
-
 
 export default class SlackHandler extends EventEmitter {
   constructor({ token }) {
