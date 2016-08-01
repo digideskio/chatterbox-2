072c29f649bf537774eb4e50fe167b5c8bb01283
Luigi Poole <luigipoole@outlook.com>
2016-08-01 16:11:09 -0700

    further style modularization

--- app/components/Chat/Message/Attachments/Author.react.js
+++ app/components/Chat/Message/Attachments/Author.react.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default ({ name, link, image, service: { image: serviceImage, name: serviceName } = {} }) => { // eslint-disable-line react/prop-types
   return (
     <div className='author-body'>
-      {serviceImage ? <div className='service-img' style={{backgroundImage: `url(${serviceImage})`}} /> : null}
+      {serviceImage ? <div className='service-image' style={{backgroundImage: `url(${serviceImage})`}} /> : null}
       {serviceName ? <div className='service-name'>{serviceName} |</div> : null}
-      {image ? <div className='auth-img' style={{backgroundImage: `url(${image})`}} /> : null}
-      {name ? <div className='auth-name'>{name}</div> : null}
+      {image ? <div className='author-image' style={{backgroundImage: `url(${image})`}} /> : null}
+      {name ? <div className='author-name'>{name}</div> : null}
     </div>
   )
 }
